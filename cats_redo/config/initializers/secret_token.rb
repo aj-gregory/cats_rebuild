a70b81b6e83ba0d51776f41919366b5cfd070162
App Academy Student <student@appacademy.io>
2013-10-12 14:54:33 -0700

    Initial commit

---
+++ cats_redo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatsRedo::Application.config.secret_token = 'fbe4ce81e6aa7d46d7c7af7586a6159d63c7d2ed291aab84953c27edf1704effc541d1d8871816fdb30f2dd1bbd0a9393fa270e4c8cb3b2c1ce21ff950977e43'
